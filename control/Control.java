535cb38e2f375442a6e954c6c9f858aab3658604
JAHemmings <jaredandreh@gmail.com>
2026-06-11 13:22:47 -0600

    Update Control.java

--- control/Control.java
+++ control/Control.java
@@ -13,9 +13,11 @@ public class Control {
 
     private Map<Integer, Usuario> usuarios;
     private Map<Integer, Item> items;
+    private Map<Integer, Prestamo> prestamos;
 
     private LinkedList<TipoItem> tipos;
     private LinkedList<Categoria> categorias;
+    private LinkedList<Alerta> alertas;
 
     private int consecutivoPrestamo = 1;
     private int consecutivoAlerta = 1;
@@ -24,7 +26,6 @@ public class Control {
 
         usuarios = new HashMap<>();
         items = new HashMap<>();
-
         tipos = new LinkedList<>();
         categorias = new LinkedList<>();
     }
@@ -33,7 +34,6 @@ public class Control {
         if (instance == null) {
             instance = new Control();
         }
-
         return instance;
     }
 
@@ -115,6 +115,98 @@ public class Control {
 
         return lista;
     }
+    
+    public Prestamo crearPrestamo(Usuario usuario) {
+
+        Prestamo prestamo = new Prestamo();
+
+        prestamo.setIdPrestamo(consecutivoPrestamo++);
+        prestamo.setUsuario(usuario);
+        prestamo.setFechaPrestamo(new Date());
+        prestamo.setEstado("ACTIVO");
+
+        prestamos.put(prestamo.getIdPrestamo(), prestamo);
+
+        usuario.agregarPrestamo(prestamo);
+
+        return prestamo;
+    }
+
+    public void agregarItemPrestamo(Prestamo prestamo, Item item) {
+
+        if (item.isDisponible()) {
+            prestamo.agregarItem(item);
+            item.marcarPrestado();
+        }
+    }
+
+    public void finalizarPrestamo(Prestamo prestamo) {
+
+        prestamo.finalizarPrestamo();
+        prestamo.setEstado("FINALIZADO");
+    }
+
+    public Prestamo obtenerPrestamo(int id) {
+        return prestamos.get(id);
+    }
+
+    public Map<Integer, Prestamo> obtenerPrestamos() {
+        return prestamos;
+    }
+
+    public void generarAlerta(Prestamo prestamo, String mensaje, String tipo) {
+
+        Alerta alerta = new Alerta();
+        alerta.setIdAlerta(consecutivoAlerta++);
+        alerta.setMensaje(mensaje);
+        alerta.setTipo(tipo);
+        alerta.setFechaAlerta(new Date());
+
+        prestamo.setAlerta(alerta);
+
+        alertas.add(alerta);
+    }
+
+    public LinkedList<Alerta> obtenerAlertas() {
+        return alertas;
+    }
+
+    public LinkedList<Item> listarItemsPrestados() {
+        LinkedList<Item> lista = new LinkedList<>();
+        for (Item item : items.values()) {
+            if (!item.isDisponible()) {
+                lista.add(item);
+            }
+        }
+
+        return lista;
+    }
+
+    public LinkedList<Prestamo> listarPrestamosActivos() {
+        LinkedList<Prestamo> lista = new LinkedList<>();
+        for (Prestamo p : prestamos.values()) {
+            if (!p.isRetornado()) {
+                lista.add(p);
+            }
+        }
+        return lista;
+    }
+
+    public LinkedList<Prestamo> listarPrestamosVencidos() {
+        LinkedList<Prestamo> lista = new LinkedList<>();
+        Date hoy = new Date();
+        for (Prestamo p : prestamos.values()) {
+            if (!p.isRetornado() && p.getFechaDevolucion() != null && p.getFechaDevolucion().before(hoy)) {
+                lista.add(p);
+            }
+        }
+        return lista;
+    }
+
+    public LinkedList<Alerta> listarAlertasPendientes() {
+        return alertas;
+    }
+
 
     public Usuario reporteUsuario(int id) {
         return usuarios.get(id);
